fd71cda9b106ea4a936fe30e0f522eaf520dce9a
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-19 08:52:57 -0700

    changing direction

--- roulette.py
+++ roulette.py
@@ -3,46 +3,53 @@ Complete one round of roulette - but if you're up to the challenge,
 feel free to build a full command line interface through which '''
 
 import random
-random.seed(1)
-bank_account = 1000
-bet_amount = 0
-bet_color = None
-bet_number = None
 
-random.seed()
-roll = random.randint(0, 37)
+bank_account = 1000
+bet = 0
+
+
+
 
 green = [0, 37]
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 
-le_wheel = [{"green": 0}, {"red":1}, {"black":2}, {"red":3}, {"black": 4}, {"red", 5}, {"black", 6}, {"red", 7}, {"black", 8}, {"red":9}, {"black":10}, {"black":11}, {"red":12},
-{"black":13}, {"red":14}, {"black":15}, {"red":16}, {"black":17}, {"red":18}, {"red":19}, {"black":20}, {"red":21}, {"black":22}, {"red":23}, {"black":24}, {"red":25}, {"black":26},
-{"red", 27}, {"black":28}, {"black":29}, {"red":30}, {"black":31}, {"red":32}, {"black":33}, {"red":34}, {"black":35}, {"red":36}, {"green":0}]
-print(le_wheel[roll])
-print(roll)
+#print("\nAllowed Bets:\nEven, Odd, Low (Numbers 1 to 18), High (Number 19 to 36), \nFirst Twelve, Second Twelve, Third Twelve, \n First Column, Second Column, Third Column\n Red, Black, or any specific number\n")
 
-la_wheelia = ["green","red","black","red","black","red","black","red","black", "red","black","black","red","black","red","black","red","black","red","red","black","red","black","red","black","red","black","red","black","black","red","black","red","black","red","black","red","green"]
-print(la_wheelia[roll])
-print(roll)
 
-def take_bet(color, number, amount):
-    bet_color = color
-    bet_number = number
-    bet_amount = amount
+le_wheel = ["green","red","black","red","black","red","black","red","black","red","black","black","red","black","red","black","red","black","red","red","black","red","black","red","black","red","black","red","black","black","red","black","red","black","red","black","red","green"]
+
+bet_space = None
+def take_bet(space, bet_amount):
+    print("bet: " + space)
+    bet_space = space
+    bet = bet_amount
+    print("space bet: " + bet_space)
+    
 
 def spin():
-    pass
+    random.seed()
+    return random.randint(0, 37)
 
 
 def check_results():
     '''Compares bet_color to color rolled.  Compares
     bet_number to number_rolled.'''
-    pass
+    print(bet_space)
+    if(bet_space == "Even"):
+        if(spin() % 2 == 0):
+            print("you won")
+        else:
+            print("Loser!")
+        
 
-def payout():
+
+def payout(leverage):
     '''returns total amount won or lost by user based on results of roll. '''
     pass
+
+
+
 
 def play_game():
     """This is the main function for the game.
@@ -54,4 +61,14 @@ def play_game():
     Determine if the user won or lost.
     Pay or deduct money from the user accordingly.
     """
-    pass
+    
+
+    spc_bet = input("Bet on a space: ")
+    bet_amt = input("Place your bet amount: ")
+    print(spc_bet)
+    take_bet(spc_bet, bet_amt)
+    check_results()
+    
+
+
+play_game()
